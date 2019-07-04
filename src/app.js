05259295a40c95de68f67dab6ae7d80e2289234a
unknown <amithy25@gmail.com>
2019-07-04 10:23:08 -0400

    Init commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+app.set('view engine','hbs')//setup handlebars.
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name : 'Amith'
+    })
+})
+
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Please provide a vaild address.'
+        })
+    }
+
+    geocode(req.query.address, (error,{latitude, longitude, location} = {})=>{
+        if(error){
+            return res.send({
+                error
+            })
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address : req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'you must provide a search term.'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me',
+        name:'Amith'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        msg:'This is an Help Message.',
+        title:'Help',
+        name:'Amith'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404Help',
+        name:'Amith',
+        errorMessage:'Help Article Not Found!'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Amith',
+        errorMessage:'Page Not Found!'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000.')
+})
